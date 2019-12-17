276712529a36d9ab19ce837ea888145045e5d74d
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-17 14:02:17 -0300

    Adding a second button

--- app/src/main/java/com/joabe/diceroller/MainActivity.kt
+++ app/src/main/java/com/joabe/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,25 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        var currentText = resultText.text.toString()
+        if (resources.getString(R.string.hello_world).equals(currentText)) {
+            currentText = "1"
+        } else if (!"6".equals(currentText)) {
+            currentText = (Integer.parseInt(currentText) + 1).toString()
+        }
+        resultText.text = currentText
     }
 }
