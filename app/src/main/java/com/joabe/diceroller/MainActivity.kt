701a3e439c34b76473f893eb0ed585b4f21fe281
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-17 15:54:59 -0300

    Finding views efficiently

--- app/src/main/java/com/joabe/diceroller/MainActivity.kt
+++ app/src/main/java/com/joabe/diceroller/MainActivity.kt
@@ -8,7 +8,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var diceImage: ImageView
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
