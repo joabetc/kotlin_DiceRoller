887bf48cebdf2c4e11031803935dd6cb8b42fbba
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-17 15:42:25 -0300

    Adding and updating image resources

--- app/src/main/java/com/joabe/diceroller/MainActivity.kt
+++ app/src/main/java/com/joabe/diceroller/MainActivity.kt
@@ -3,47 +3,36 @@ package com.joabe.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultText: TextView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultText = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-        val countUpButton: Button = findViewById(R.id.countUp_button)
-        countUpButton.setOnClickListener { countUp() }
-
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getImageResource(randomInt))
     }
 
-    private fun countUp() {
-        var currentText = resultText.text.toString()
-        if (resources.getString(R.string.hello_world).equals(currentText)) {
-            currentText = "1"
-        } else if (!"6".equals(currentText)) {
-            currentText = (Integer.parseInt(currentText) + 1).toString()
+    private fun getImageResource(randomInt: Int): Int {
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        resultText.text = currentText
-    }
-
-    private fun reset() {
-        resultText.text = "0"
     }
 }
